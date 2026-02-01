a9ae0ab51dcb5decc346a3220a4854537e745a59
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2026-02-01 18:06:15 +0400

    add and configure logback core+classic. add logging

--- src/main/java/org/example/UserDao.java
+++ src/main/java/org/example/UserDao.java
@@ -2,20 +2,27 @@ package org.example;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.function.BiConsumer;
 
 public class UserDao implements Dao<User> {
+    private final Logger logger
+            = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public void save(User user) {
+        logger.info("Saving user with parameters: {}", user);
         BiConsumer<Session, User> saveOperation = Session::persist;
         dbSingularOperationLogic(user, saveOperation);
     }
 
     @Override
     public Optional<User> get(long id) {
+        logger.info("Trying to find user with ID: {}", id);
         try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
             return Optional.ofNullable(session.find(User.class, id));
         }
@@ -23,6 +30,7 @@ public class UserDao implements Dao<User> {
 
     @Override
     public List<User> getAll() {
+        logger.info("Get all users from the table.");
         try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
             return (List<User>) session.createQuery("FROM User u").list();
         }
@@ -30,12 +38,14 @@ public class UserDao implements Dao<User> {
 
     @Override
     public void update(User user) {
+        logger.info("Updating the user with parameters: {}", user);
         BiConsumer<Session, User> mergeOperation = Session::merge;
         dbSingularOperationLogic(user, mergeOperation);
     }
 
     @Override
     public void delete(User user) {
+        logger.info("Deleting user: {}", user);
         BiConsumer<Session, User> removeOperation = Session::remove;
         dbSingularOperationLogic(user, removeOperation);
     }
@@ -51,6 +61,8 @@ public class UserDao implements Dao<User> {
             dbOperation.accept(session, user);
             session.getTransaction().commit();
         } catch (RuntimeException e) {
+            logger.error("Exception operating on entity {}\nError class: {}\nError message: {}"
+                    , user, e.getClass(), e.getMessage());
             if (tx != null) tx.rollback();
             throw e;
         } finally {
