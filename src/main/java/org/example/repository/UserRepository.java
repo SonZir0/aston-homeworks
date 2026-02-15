66a922ca4c729dcd4cf8397252432be0da4110fe
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2026-02-15 07:58:52 +0400

    replace UserDao with UserRepository

---
+++ src/main/java/org/example/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package org.example.repository;
+
+import org.example.models.User;
+import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByName(String name);
+    List<User> findAllByName(String name);
+}
