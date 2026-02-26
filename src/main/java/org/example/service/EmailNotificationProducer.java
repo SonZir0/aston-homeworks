b10f9c5be552d06f2b19954e2a25da3602f3762b
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2026-02-26 19:40:06 +0400

    move topic name to the application.properties file

--- src/main/java/org/example/service/EmailNotificationProducer.java
+++ src/main/java/org/example/service/EmailNotificationProducer.java
@@ -1,17 +1,20 @@
 package org.example.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailNotificationProducer {
+    @Value("${app.kafka.topics.my-topic}")
+    private String topicName;
 
     @Autowired
     KafkaTemplate<String, String[]> template;
 
     public void sendEmailNotification(String email, String message) {
-        template.send("emailNotifications",
+        template.send(topicName,
                 new String[]{email, message});
     }
 }
