7512d0feef96eba337748aff4cf52f5cc5862dca
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2026-03-13 15:41:08 +0400

    fix CORS issue

--- notification-service/src/main/java/org/example/notification_service/controller/SendMailApi.java
+++ notification-service/src/main/java/org/example/notification_service/controller/SendMailApi.java
@@ -4,13 +4,11 @@ import jakarta.validation.Valid;
 import org.example.notification_service.dto.SendEmailRequestDto;
 import org.example.notification_service.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/")
+@CrossOrigin("*")
 public class SendMailApi {
     EmailService emailService;
 
